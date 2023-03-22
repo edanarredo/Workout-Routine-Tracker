caa0e09b22ddd531fb1d8976e8ee501b0feab922
Daniel <edanielarredondo@gmail.com>
2023-03-22 18:17:57 -0500

    Change to workout routes to implement controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,37 +1,19 @@
 const express = require("express");
-const { createWorkout, listWorkouts, getWorkoutById, deleteWorkoutById, updateWorkoutById } = require("../controllers/workout/workoutController");
+const { 
+    createWorkout, 
+    listWorkouts, 
+    getWorkoutById, 
+    deleteWorkoutById, 
+    updateWorkoutById 
+} = require("../controllers/workout/workoutController");
 
 // Creates an instance of the router for us.
 const router = express.Router();
 
-// Get all workouts
-router.get('/', (req, res) => {
-    res.json({ message: 'Get all workouts.' });
-});
-
-// Get a single workout
-router.get("/", (req, res) => {
-  res.json({ message: "Get a single workout" });
-});
-
-// Get a single workout by id
-router.get("/:id", (req, res) => {
-  res.json({ message: `Get a single workout by ID` });
-});
-
-// Create a new workout
-router.post("/", async (req, res) => {
-  
-});
-
-// Delete a workout by id
-router.delete("/:id", (req, res) => {
-  res.json({ message: `Delete a workout by ID` });
-});
-
-// Update a workout by id
-router.patch("/:id", (req, res) => {
-  res.json({ message: `Update a workout by ID` });
-});
+router.get('/', async (req, res) => { listWorkouts(req, res) });
+router.get("/:id", async (req, res) => { getWorkoutById(req, res) });
+router.post("/", async (req, res) => { createWorkout(req, res) });
+router.delete("/:id", async (req, res) => { deleteWorkoutById(req, res) });
+router.patch("/:id", async (req, res) => { updateWorkoutById(req, res) });
 
 module.exports = router;
